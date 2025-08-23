d8aed62e0f006fcb54995fe107ee04d6c4d5274d
Aakash Ojha <aakashojha68@gmail.com>
2025-08-23 09:59:00 +0530

    added frontend url env - AO

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,7 +6,7 @@ const Message = require("../model/message");
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
     },
   });
 
